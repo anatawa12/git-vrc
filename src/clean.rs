941578ca755fa594a05804ad917ad026eef844f5
anatawa12 <anatawa12@icloud.com>
2022-07-04 15:03:33 +0900

    fix: this crashes with stripped object

--- src/clean.rs
+++ src/clean.rs
@@ -15,6 +15,19 @@ impl App {
     pub(crate) fn run(self) -> anyhow::Result<()> {
         let mut yaml = String::new();
         stdin().read_to_string(&mut yaml)?;
+         let mut iter = YamlSeparated::new(&yaml);
+        let first = iter.next().unwrap();
+        print!("{}{}", first.0, first.1);
+
+        while let Some((heading, body)) = iter.next() {
+            print!("{}", heading);
+            self.parse_one(body)?;
+        }
+
+        Ok(())
+    }
+
+    fn parse_one(&self, yaml: &str) -> anyhow::Result<()> {
         let mut parser = Parser::new(yaml.chars());
 
         let mut states = Vec::<Box<dyn EventReceiver>>::new();
@@ -54,6 +67,65 @@ impl App {
         }
         print!("{}", context.finish());
         Ok(())
+    }
+}
+
+struct YamlSeparated<'a> {
+    str: &'a str,
+}
+
+impl <'a> YamlSeparated<'a> {
+    fn new(str: &'a str) -> Self {
+        Self { str }
+    }
+}
+
+impl<'a> Iterator for YamlSeparated<'a> {
+    type Item = (&'a str, &'a str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.str.len() == 0 {
+            return None
+        }
+
+        let heading_line;
+        if !self.str.starts_with("---") {
+            // heading element: no heading line
+            heading_line = "";
+        } else {
+            let rest;
+            if let Some(lf) = self.str.find('\n') {
+                (heading_line, rest) = self.str.split_at(lf + 1)
+            } else {
+                (heading_line, rest) = (self.str, "")
+            }
+            self.str = rest;
+        }
+
+        let str_in = self.str;
+        let mut i = 0;
+
+        loop {
+            if let Some(triple_hyphen) = self.str.find("---") {
+                if self.str[..triple_hyphen].chars().last() == Some('\n') {
+                    // it's separator!
+                    i += triple_hyphen;
+                    break
+                } else {
+                    // it's not a separator. find next
+                    i += triple_hyphen;
+                    let (_, post) = self.str.split_at(triple_hyphen + 3);
+                    self.str = post;
+                }
+            } else {
+                i = self.str.len();
+                // there's no separator!
+                break
+            }
+        }
+        self.str = &str_in[i..];
+
+        return Some((heading_line, &str_in[..i]))
     }
 }
 
